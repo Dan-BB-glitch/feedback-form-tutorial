feabd3b6865ce274392cd7fcc69a4713fb918fc2
boyeded5@hwbcymru.net <boyeded5@hwbcymru.net>
2026-06-11 09:19:45 +0000

    Created LICENSE.txt; Edited settings and Form1; Deleted ServerModule1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -12,20 +12,8 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-def clear_inputs(self):
-  self.name_box.text = ""
-  self.email_box.text = ""
-  self.feedback_box.text = ""
-  
-  @handle("submit_button", "click")
-  def submit_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    name = self.name_box.text
-    email = self.email_box.text
-    feedback = self.feedback_box.text
-    anvil.server.call('add_feedback', name, email, feedback)
-    Notification("Feedback submitted!").show()
-    self.clear_inputs()
+
+
 
 
 
